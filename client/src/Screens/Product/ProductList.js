1eb270fedbd8ff35e45b0d67d3e0a5c1196c950a
SathishNagula037 <sathish.nagula037@gmail.com>
2023-08-21 11:09:39 +0530

    canged

--- client/src/Screens/Product/ProductList.js
+++ client/src/Screens/Product/ProductList.js
@@ -6,32 +6,70 @@ import { DataGrid } from '@mui/x-data-grid';
 
 
 function ProductList() {
-    const dispatch = useDispatch();
-    const { loading, error, products, message } = useSelector(state => state.productList);
+  const dispatch = useDispatch();
+  const { loading, error, products, message } = useSelector(state => state.productList);
 
 
 
-    useEffect(() => {
-        dispatch(listProduct());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(listProduct());
+  }, [dispatch]);
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'ProductName',
+      headerName: 'ProductName',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'ProductDetail',
+      headerName: 'ProductDetail',
+      width: 150,
+      editable: true,
+    },
+
+    {
+      field: 'ProductPrice',
+      headerName: 'ProductPrice',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+  const rows = products.map((product) => ({
+    id: product.id,
+    ProductName: product.name,
+    ProductDetail: product.ProductDetail,
+    ProductPrice: product.ProductPrice,
+    ProductAvailable: product.ProductAvailable
+
+  }));
 
 
+  return (
 
-  
-            return (
-          
-                    <div>
-                      {products && products.map((product) => (
-                        <p key={product.id}>{product.name}</p>
-                      ))}
-                    </div>
-               
-            );
-   
+
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+
+    </Box>
+  );
+
 
 }
 
